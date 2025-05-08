7d43ae2cde77fa6fb6140a15ad76e6e31a51df8b
AkashSingh062 <akash622003singh@gmail.com>
2025-05-08 19:38:24 +0530

    -

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get('/', (req, res) => {
 app.get('/twitter', (req, res) => {
     res.send('Hello Twitter!');
 });
+app.get('/youtube', (req, res) => {
+    res.send('<h1>Hello Youtube!</h1>');
+});
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
